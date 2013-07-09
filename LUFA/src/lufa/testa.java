a7934531e59a82a4b9a1412c9c8f26d563349589
Fabian <Fabian@Fabian-Thinkpad.home>
2013-07-09 20:54:00 +0200

    comi

---
+++ LUFA/src/lufa/testa.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lufa;
+
+/**
+ *
+ * @author Fabian
+ */
+public class testa {
+    
+}
